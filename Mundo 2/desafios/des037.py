8a64667c60fb847e60daab65aa2a0ca676836e40
mayconasilva <mayconsilva0381@gmail.com>
2021-09-27 09:39:19 -0300

    Correção de melhorias

--- Mundo 2/desafios/des037.py
+++ Mundo 2/desafios/des037.py
@@ -1,8 +1,8 @@
 num = int(input("Digite um número inteiro"))
 opção = int(input("Escolha [1] para binário, [2] para Octal e [3] para hexadecimal"))
 if (opção == 1):
-    print(f'{num} equivale a {bin(num)} em binário')
+    print(f'{num} equivale a {bin(num)[2:]} em binário')
 elif (opção == 2):
-    print(f'{num} equivale a {oct(num)} em Octal') 
+    print(f'{num} equivale a {oct(num)[2:]} em Octal') 
 else:
-    print(f'{num} equivale a {hex(num)} em hexadecimal')
+    print(f'{num} equivale a {hex(num)[2:]} em hexadecimal')
